c9d27e16195e0889e2872b6dea4855ca1b42cee9
Fer0998 <estebanfernandoalvarez@gmail.com>
2023-08-08 16:54:06 -0300

    Initial commit

---
+++ src/javaapplication65/Nombres.java
@@ -0,0 +1,33 @@
+
+package javaapplication65;
+
+
+public class Nombres {
+    
+    private String nombre01;
+    private String nombre02;
+
+    public Nombres(String nombre01, String nombre02) {
+        this.nombre01 = nombre01;
+        this.nombre02 = nombre02;
+    }
+
+    public String getNombre01() {
+        return nombre01;
+    }
+
+    public void setNombre01(String nombre01) {
+        this.nombre01 = nombre01;
+    }
+
+    public String getNombre02() {
+        return nombre02;
+    }
+
+    public void setNombre02(String nombre02) {
+        this.nombre02 = nombre02;
+    }
+    
+    
+    
+}
---
+++ src/javaapplication65/Prueba.java
@@ -0,0 +1,18 @@
+
+package javaapplication65;
+
+import javax.swing.JOptionPane;
+
+public class Prueba {
+
+
+    public static void main(String[] args) {
+        
+     
+        String nom = JOptionPane.showInputDialog(null,"Ingrese su nombre " );
+        
+        JOptionPane.showInternalMessageDialog(null,"nom");
+   
+    }
+ 
+}
